88749d82ba5be438975dcc861d593103aa2a0f0d
rapid trader <166290591+rapidtrader@users.noreply.github.com>
2026-04-03 18:20:47 +0530

    change the name of documents to employee documents

--- src/lib/getEmpCrmAdminSidebarMenuItems.js
+++ src/lib/getEmpCrmAdminSidebarMenuItems.js
@@ -14,7 +14,7 @@ const empCrmMenuItems = [
 
   { path: "/empcrm/admin-dashboard/attendance-summary", name: "Attendance Summary", roles: ["SUPERADMIN", "HR HEAD", "HR", "HR Executive"], icon: "LayoutGrid" },
   { path: "/empcrm/admin-dashboard/attendance", name: "Attendance details", roles: ["SUPERADMIN", "HR HEAD", "HR", "HR Executive"], icon: "Clock" },
-  { path: "/empcrm/admin-dashboard/documents", name: "Documents", roles: ["SUPERADMIN", "HR HEAD", "HR", "HR Executive"], icon: "FileText" },
+  { path: "/empcrm/admin-dashboard/documents", name: "Employee Documents", roles: ["SUPERADMIN", "HR HEAD", "HR", "HR Executive"], icon: "FileText" },
   { path: "/empcrm/admin-dashboard/salary", name: "Salary Management", roles: ["SUPERADMIN", "HR HEAD", "HR", "HR Executive"], icon: "DollarSign" },
   { path: "/empcrm/admin-dashboard/salary-slips", name: "Salary slips", roles: ["SUPERADMIN", "HR HEAD", "HR", "HR Executive", "ACCOUNTANT"], icon: "Receipt" },
 ];
